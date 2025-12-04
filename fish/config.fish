436927d0ca529d0d63d7afa867902decb8d7fae9
wncfht <2130212584@qq.com>
2025-12-04 12:52:55 +0800

    init

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-07-31 18:07:53
+set PATH $PATH /Users/zhangkangning/.local/bin
